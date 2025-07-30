bdf39cb496759b6bd6e6374a4a57122d1b33f3f8
DTBknight <Hugecheshirekat@gmail.com>
2025-07-31 00:01:11 +0800

    修复车型图片卡片标题和图标被遮挡的问题 - 提高z-index层级确保标题显示在最前面

--- js/carSearch.js
+++ js/carSearch.js
@@ -391,7 +391,7 @@ export class CarSearch {
     if (carMainImageBox && (carData.image || carData.mainImage)) {
       const imageUrl = carData.image || carData.mainImage;
       const imageAlt = carData.name || carData.carName;
-      carMainImageBox.innerHTML = `<img src="${imageUrl}" alt="${imageAlt}" class="w-full h-full object-contain rounded bg-gray-50 scale-150">`;
+      carMainImageBox.innerHTML = `<img src="${imageUrl}" alt="${imageAlt}" class="w-full h-full object-contain rounded bg-gray-50 scale-150 -z-10">`;
     } else {
       // 如果没有图片，显示占位符
       carMainImageBox.innerHTML = `
